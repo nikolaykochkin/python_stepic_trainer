c908cbab4d018abbe6554b2a3ff88e463e32f500
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-07-02 23:31:37 +0300

    3.8 done

--- 3.8 Spiral matrix.py
+++ 3.8 Spiral matrix.py
@@ -1,29 +1,14 @@
 n = int(input())
-lst = [[0 for i in range(n)] for i in range(n)]
+lst = [[0] * n for i in range(n)]
 i = j = di = 0
 dj = cnt = 1
-max = n - 1
+
 while cnt <= n ** 2:
-    for y in range(4):
-        for z in range(max):
-            lst[i][j] = str(cnt)
-            cnt += 1
-            i += di
-            j += dj
-
-        if dj == 1:
-            di, dj = 1, 0
-        elif di == 1:
-            di, dj = 0, -1
-        elif dj == -1:
-            di, dj = -1, 0
-            max -= 1
-        else:
-            di, dj = 0, 1
-    lst[i][j] = str(cnt)
-    j += 1
+    lst[i][j] = cnt
+    if lst[(i + di) % n][(j + dj) % n]:
+        di, dj = dj, -di
+    i, j = i + di, j + dj
     cnt += 1
-    max -= 1
 
-for i in range(n):
-    print('\t'.join(lst[i]))
+for l in lst:
+    print(*l)
